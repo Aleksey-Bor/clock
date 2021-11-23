317f709c6034bc69301ee933b3d2cf162334e528
Aliaksey <it.borki@gmail.com>
2021-11-23 18:09:42 +0300

    Create Clock Face and numbers

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hi");
+"use script";
+
+let accuracy = 1000;
+
+let Watch = {
+  watchParts: { clockFace: null, number: null },
+
+  init(accuracy) {
+    this.watchParts.clockFace = document.createElement("div");
+    
+    this.watchParts.clockFace.classList.add("clock_face");
+    for (let i = 0; i < 12; i++) {
+      this.watchParts.number = document.createElement("div");
+      this.watchParts.number.classList.add("number");
+      this.watchParts.clockFace.append(this.watchParts.number);
+    }
+    document.body.append(this.watchParts.clockFace);
+    console.log(this.watchParts.clockFace);
+
+
+  },
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  Watch.init(accuracy);
+});
