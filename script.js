f578d428dc9d69347a58f3098777c09a525ab9c5
Aliaksey <it.borki@gmail.com>
2021-11-25 13:44:50 +0300

    Position the numbers in the dial

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 "use script";
 
-let accuracy = 1000;
+let accuracy = 10000;
 
 let Watch = {
-  watchParts: { clockFace: null, number: null },
+  watchProperties: {
+    clockFaceSize: 300,
+    digitSize: 10,
+    countDigits: 12,
+  },
 
   init(accuracy) {
-    this.watchParts.clockFace = document.createElement("div");
+    let clockFace = document.createElement("div");
+    clockFace.classList.add("clock_face");
+    clockFace.style.width = this.watchProperties.clockFaceSize + "px";
+    clockFace.style.height = this.watchProperties.clockFaceSize + "px";
     
-    this.watchParts.clockFace.classList.add("clock_face");
-    for (let i = 0; i < 12; i++) {
-      this.watchParts.number = document.createElement("div");
-      this.watchParts.number.classList.add("number");
-      this.watchParts.clockFace.append(this.watchParts.number);
+    let wrapperForDigits = document.createElement("div");
+    wrapperForDigits.classList.add("wrapper_digits");
+    let radiusDigitsCircle = this.watchProperties.clockFaceSize / 2 - 20;
+    wrapperForDigits.style.height = radiusDigitsCircle * 2 + "px";
+    wrapperForDigits.style.width = radiusDigitsCircle * 2 + "px";
+
+    for (let i = 0; i < this.watchProperties.countDigits; i++) {
+      let digit = document.createElement("div");
+      digit.classList.add("digit");
+      digit.style.width = this.watchProperties.digitSize + "px";
+      digit.style.height = this.watchProperties.digitSize + "px";
+      let angleEachDigitRadians =
+        (2 / this.watchProperties.countDigits) * i * Math.PI;
+      let left = (radiusDigitsCircle * Math.sin(angleEachDigitRadians) + radiusDigitsCircle) + "px";
+      let top = (radiusDigitsCircle * Math.cos(angleEachDigitRadians) + radiusDigitsCircle) + "px";
+      digit.style.left = left;
+      digit.style.top = top;
+      wrapperForDigits.append(digit);
     }
-    document.body.append(this.watchParts.clockFace);
-    console.log(this.watchParts.clockFace);
-
-
+    clockFace.append(wrapperForDigits);
+    document.body.append(clockFace);
   },
 };
 
