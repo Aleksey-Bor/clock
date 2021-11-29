6d30dbb60dd04783cccc22a0a5063464ecf29ef0
Aliaksey <it.borki@gmail.com>
2021-11-29 20:27:11 +0300

    Final test5

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ let Watch = {
       let secondsString = `${seconds < 10 ? "0" + seconds : seconds}`;
       this.watchProperties.electronicDial.innerHTML = `<div>${hoursString}</div> : <div>${minutesString}</div> : <div>${secondsString}</div>`;
 
-      if (month === 10 && date === 29 && hours === 20 && minutes >= 15 && minutes < 21) {
+      if (month === 10 && date === 30 && hours === 0 && minutes >= 0 && minutes < 6) {
         body.className = "festive";
         congratulationText.className = "congratulation";
         // let minutes = time.getMinutes();
@@ -156,7 +156,7 @@ let Watch = {
     };
 
     _greetingSongPlay = (minutes, seconds) => {
-      if (minutes === 15 && seconds < 1) {
+      if (minutes === 0 && seconds < 1) {
         greetingSong.play();
       }
     };
